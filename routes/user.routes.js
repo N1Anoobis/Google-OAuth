87781bed3c3c33c0b379f6738a00659c8827fd14
N1Anoobis <mrbond84@hotmail.com>
2020-10-08 15:49:10 +0200

    Add middleweare still need work

---
+++ routes/user.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const isLogged = (req, res, next) => {
+  if(!req.user){
+    res.redirect('/no-permission');
+  } else {
+    next();
+  }
+};
+
+router.get('/logged', isLogged, (req, res) => {
+  res.render('logged');
+});
+
+router.get('/profile/',isLogged, (req, res) => {
+        res.render('profile');
+    });
+
+router.get('/profile/settings',isLogged, (req, res) => {
+        res.render('settings');
+    });
+
+router.get('/no-permission', (req, res) => {
+  res.render('noPermission');
+});
+
+module.exports = router;
